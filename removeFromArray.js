fa0c6792de5f0aa30723750102b44ef03c6e59da
PhilMackie <phil.mackie9@gmail.com>
2021-09-23 16:39:19 -0500

    sending to colleague

--- removeFromArray.js
+++ removeFromArray.js
@@ -1,5 +1,8 @@
-const removeFromArray = function() {
-
+const str = [1, 2, 3, 4];
+const removeFromArray = function(i) {
+const newStr = str.replace('i',"");
+console.log(newStr);
 };
+removeFromArray(2);
 
 module.exports = removeFromArray;
