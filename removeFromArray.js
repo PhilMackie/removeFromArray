7402b1216b815aa285f23c26c7f47e55c954f91f
PhilMackie <phil.mackie9@gmail.com>
2021-09-23 17:07:09 -0500

    sad

--- removeFromArray.js
+++ removeFromArray.js
@@ -1,6 +1,7 @@
 const str = [1, 2, 3, 4];
 const removeFromArray = function(i) {
-const newStr = str.replace('i',"");
+const strLocation = str.indexOf(i);
+const newStr = str.splice(strLocation, " ");
 console.log(newStr);
 };
 removeFromArray(2);
