7378ded3ca2d92b8fad0cf1f222483a322ab9bc1
PhilMackie <phil.mackie9@gmail.com>
2021-09-26 17:32:47 -0500

    1 of 6 done

--- removeFromArray.js
+++ removeFromArray.js
@@ -1,9 +1,18 @@
 const str = [1, 2, 3, 4];
-const removeFromArray = function(i) {
-const strLocation = str.indexOf(i);
-const newStr = str.splice(strLocation, " ");
-console.log(newStr);
-};
-removeFromArray(2);
+const i = 3; 
+const valueTwo = 2;
+/// write more than one function , one to chekc and one to remove
+// https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Array/splice
 
+const removeFromArray = function() {
+//find indexOf
+const index = str.indexOf(i);
+console.log(index);
+//splice it at indexOf
+    str.splice(index, 1);
+    console.log(str);
+    return str;
+};
+
+// Do not edit below this line
 module.exports = removeFromArray;
